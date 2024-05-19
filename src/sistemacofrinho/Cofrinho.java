b7e93e0a1d345002e6fccc8132ea102fa29ad611
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-19 17:11:03 -0300

    Criado metodo para retornar listagem das moedas

--- src/sistemacofrinho/Cofrinho.java
+++ src/sistemacofrinho/Cofrinho.java
@@ -1,6 +1,7 @@
 package sistemacofrinho;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -18,16 +19,20 @@ public class Cofrinho {
 		this.listaMoedas = listaMoedas;
 	}
 
-	public void adicionar(Moeda moeda) {
+	public Boolean adicionar(Moeda moeda) {
 		Boolean moedaValorValido = moeda.verificaValorValido();
 		if (moedaValorValido == true) {
 			listaMoedas = this.getListaMoedas();
 			listaMoedas.add(moeda);
 			this.setListaMoedas(listaMoedas);
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 	
-	public void remover(Moeda moeda) {
+	public Boolean remover(Moeda moeda) {
 		double epsilon = 0.0001;
 		listaMoedas = this.getListaMoedas();
 		List<Moeda> moedasComValorBuscado = listaMoedas.stream()
@@ -38,14 +43,33 @@ public class Cofrinho {
 			Moeda moedaParaRemover = moedasComValorBuscado.get(0);
 			listaMoedas.remove(moedaParaRemover);
 			this.setListaMoedas(listaMoedas);
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 	
 	public String listagemMoedas() {
-//		
+		StringBuilder stringMoedasAgrupadasPorNomeValor = new StringBuilder();
+		stringMoedasAgrupadasPorNomeValor.append("Listagem de moedas");
+		listaMoedas = this.getListaMoedas();
+		Map<String, Map<Double, Long>> moedasAgrupadasComContagem = listaMoedas.stream()
+				.collect(Collectors.groupingBy(Moeda::getNomeMoeda, 
+						Collectors.groupingBy(Moeda::getValor,
+								Collectors.counting())));
+		moedasAgrupadasComContagem.forEach((nomeMoeda, mapValorContagem) -> 
+		{
+			stringMoedasAgrupadasPorNomeValor.append("Moeda:").append(nomeMoeda).append("\n");
+			mapValorContagem.forEach((valor, contagem) -> {
+				stringMoedasAgrupadasPorNomeValor.append("\tValor: ").append(valor).append(" - Quantidade:")
+				.append(contagem).append("\n");
+			});
+		});
+		return stringMoedasAgrupadasPorNomeValor.toString();
 	}
 	
-	public double totalConvertido(String paraQualMoeda) {
-		
-	}
+//	public double totalConvertido(String paraQualMoeda) {
+//		
+//	}
 }
