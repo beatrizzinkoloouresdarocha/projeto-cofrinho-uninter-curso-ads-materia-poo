7fdedfb2d1c56bf3cf97cc5247d9f18f6f6d8bc5
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-05-20 20:53:07 -0300

    Adicionados comentarios no codigo da classe Principal e da classe Cofrinho

--- src/sistemacofrinho/Cofrinho.java
+++ src/sistemacofrinho/Cofrinho.java
@@ -6,19 +6,20 @@ import java.util.stream.Collectors;
 
 
 public class Cofrinho {
-	private ArrayList<Moeda> listaMoedas = new ArrayList<Moeda>();
+	//classe com os métodos  e atributos do cofrinho de moedas 
+	private ArrayList<Moeda> listaMoedas = new ArrayList<Moeda>();//lista de moedas do cofrinho 
 
-	public ArrayList<Moeda> getListaMoedas() {
+	public ArrayList<Moeda> getListaMoedas() { //obter lista de moedas do cofrinho 
 		return listaMoedas;
 	}
 	
-	public void setListaMoedas(ArrayList<Moeda> listaMoedas) {
+	public void setListaMoedas(ArrayList<Moeda> listaMoedas) {//difinir  lista de moedas  do cofrinho 
 		this.listaMoedas = listaMoedas;
 	}
 
-	public Boolean adicionar(Moeda moeda) {
-		Boolean moedaValorValido = moeda.verificaValorValido();
-		if (moedaValorValido == true) {
+	public Boolean adicionar(Moeda moeda) { //método para adicionar um moeda por vez no cofrinho 
+		Boolean moedaValorValido = moeda.verificaValorValido(); //verifica se o valor é válido para essa moeda
+		if (moedaValorValido == true) { //adiciona moeda na lista de moedas do cofrinho
 			listaMoedas = this.getListaMoedas();
 			listaMoedas.add(moeda);
 			this.setListaMoedas(listaMoedas);
@@ -29,14 +30,15 @@ public class Cofrinho {
 		}
 	}
 	
-	public Boolean remover(Moeda moeda) {
-		double epsilon = 0.0001;
-		listaMoedas = this.getListaMoedas();
+	public Boolean remover(Moeda moeda) { //método para remover uma moeda especifica  do cofrinho 
+		double epsilon = 0.0001; //valor auxiliar utilizado na comparação de igualdade de valores monetários 
+		listaMoedas = this.getListaMoedas(); //obter lista de moedas  do cofrinho
+		//busca se na lista de moedas do cofrinho existe essa moeda especifica (nome e valor)
 		List<Moeda> moedasComValorBuscado = listaMoedas.stream()
 				.filter(moedaNaLista -> moedaNaLista.getNomeMoeda().equals(moeda.getNomeMoeda()))
 				.filter(moedaNaLista -> Math.abs(moedaNaLista.getValor() - moeda.getValor()) < epsilon)
 				.collect(Collectors.toList());
-		if (!moedasComValorBuscado.isEmpty()) {
+		if (!moedasComValorBuscado.isEmpty()) { //se a moeda especifica foi encontrada no cofrinho, remove ela do cofrinho
 			Moeda moedaParaRemover = moedasComValorBuscado.get(0);
 			listaMoedas.remove(moedaParaRemover);
 			this.setListaMoedas(listaMoedas);
@@ -47,29 +49,33 @@ public class Cofrinho {
 		}
 	}
 	
-	public String listagemMoedas() {
+	public String listagemMoedas() { 
+		// método que retorna  a listagem de moedas do cofrinho agrupada por nome da moeda e valor, e exibe  quantidade de cada moeda
 		StringBuilder stringMoedasAgrupadasPorNomeValor = new StringBuilder();
 		stringMoedasAgrupadasPorNomeValor.append("Listagem de moedas \n\n");
-		listaMoedas = this.getListaMoedas();
+		listaMoedas = this.getListaMoedas(); //obter lista de moedas  do cofrinho 
+		//agrupa as moedas por nome e valor e conta  a quantidade de cada moeda/valor
 		Map<String, Map<Double, Long>> moedasAgrupadasComContagem = listaMoedas.stream()
 				.collect(Collectors.groupingBy(Moeda::getNomeMoeda, 
 						Collectors.groupingBy(Moeda::getValor,
 								Collectors.counting())));
 		moedasAgrupadasComContagem.forEach((nomeMoeda, mapValorContagem) -> 
-		{
+		{ //para cada moeda/valor mota uma linha na string
 			stringMoedasAgrupadasPorNomeValor.append("Moeda:").append(nomeMoeda).append("\n");
 			mapValorContagem.forEach((valor, contagem) -> {
 				stringMoedasAgrupadasPorNomeValor.append("\tValor: ").append(valor).append(" - Quantidade:")
 				.append(contagem).append("\n");
 			});
 		});
-		return stringMoedasAgrupadasPorNomeValor.toString();
+		//retorna lista de moedas  agrupada  por moeda/valor e contendo a quantidade de cada grupo
+		return stringMoedasAgrupadasPorNomeValor.toString(); 
 	}
 	
-	public Double totalConvertido() {
+	public Double totalConvertido() { 
+		//método para retornar total das moedas do cofrinho convertidas para Real
 		final double[] total = {0};
-		listaMoedas = this.getListaMoedas();
-		listaMoedas.forEach(moeda -> total[0] += moeda.converter("Real"));
-		return total[0];
+		listaMoedas = this.getListaMoedas(); //obter lista de moedas do cofrinho 
+		listaMoedas.forEach(moeda -> total[0] += moeda.converter("Real")); //coverte cada moeda do cofrinho para Real
+		return total[0]; //retorna total das moesdas do cofrinho covertidas para Real
 	}
 }
--- src/sistemacofrinho/Principal.java
+++ src/sistemacofrinho/Principal.java
@@ -6,126 +6,132 @@ import java.util.stream.Collectors;
 
 public class Principal {
 
+	//Essa é a classe principal que contém o método main que executa o sistema do cofrinho 
+	
 	public static void main(String[] args) {
-		Cofrinho cofrinho = new Cofrinho();
+		Cofrinho cofrinho = new Cofrinho(); //instancia da classe cofrinho 
 		int opcaoMenuPrincipal;
 		int moedaEscolhida;
 		double valor;
 		Moeda moeda = null;
 		
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in); //instancia da classe Scanner é usada pra ler o que o usuário digitar 
 		try {
-			while (true) {
+			while (true) { //para executar o sistema em loop até que o usuário digite a opção  0-encerrar
 				System.out.println("Digite a opção desejada: \n 1-Adicionar moeda \n 2-Remover moeda \n 3-Listar moedas \n " + 
 									"4-Calcular total convertido \n 0-Encerrar");
 				
 				try {
-					opcaoMenuPrincipal = scanner.nextInt();
+					opcaoMenuPrincipal = scanner.nextInt(); //ler o proximo número digitado 
 				}
-				catch (InputMismatchException e) {
+				catch (InputMismatchException e) { //tratamento de erro caso o usúario não digite um número
 					System.out.println("O valor digitado é inválido, digite apenas números.");
-					scanner.nextLine();
-					continue;
+					scanner.nextLine(); //para avançar uma linha no scanner e não da erro 
+					continue; //voltando ao inicio do loop 
 				}
 				
 				if (opcaoMenuPrincipal == 1) { //adicionar moeda
-					while (true) {
+					while (true) { //executar em loop até que seja selecionada uma opção válida 
 						System.out.println("Digite a opção desejada: \n 1-Real \n 2-Dólar \n 3-Euro");
-						moedaEscolhida = scanner.nextInt();
+						moedaEscolhida = scanner.nextInt(); //ler a opção digitada pelo usúario 
 						System.out.println("Digite o valor:");
 						try {
-							valor = scanner.nextDouble();
+							valor = scanner.nextDouble(); //ler o valor digitado pelo usúario 
 						}
-						catch (InputMismatchException e) {
+						catch (InputMismatchException e) { //tratamento de erro para caso ou usúario digite um valor inválido
 							System.out.println("O valor digitado é inválido, digite apenas números." + 
 											  "Utilize a vírgula como separador decimal.");
-							scanner.nextLine();
-							continue;
+							scanner.nextLine();  //para avançar uma linha no scanner e não dar erro 
+							continue; //voltando ao inicio do loop 
 						}
-						if (moedaEscolhida == 1) {
-							moeda = new Real(valor);
+						if (moedaEscolhida == 1) { 
+							moeda = new Real(valor); //cria uma moeda de Real com o valor passado pelo usúario 
 						}
 						else if (moedaEscolhida == 2) {
-							moeda = new Dolar(valor);
+							moeda = new Dolar(valor); //cria uma moeda de Dolar com o valor passado pelo usúario 
 						}
 						else if (moedaEscolhida == 3) {
-							moeda = new Euro(valor);
+							moeda = new Euro(valor); //cria uma moeda de Euro com o valor passado pelo usúario 
 						}
 						
-						if (moedaEscolhida >= 1 && moedaEscolhida <=3) {
-							Boolean adicionadaComSucesso = cofrinho.adicionar(moeda);
-							if (adicionadaComSucesso == true) {
+						if (moedaEscolhida >= 1 && moedaEscolhida <=3) { //se foi selecionada uma opção válida 
+							Boolean adicionadaComSucesso = cofrinho.adicionar(moeda); //adiciona moeda no cofrinho 
+							if (adicionadaComSucesso == true) { //verefica se moeda foi adicionada com sucesso no cofrinho 
 								System.out.println("Moeda adicionada com sucesso!");
 							}
-							else {
+							else { //exibe uma mensagem ao o usúario informando que o valor digitado é invalido e 
+								//e informa  os valores válidos para a  moeda selecionada 
 								List<Double> moedaValoresValidos = moeda.getValoresDeMoedaValidos();
 								StringBuilder stringDigiteValoresMoedaValidos = new StringBuilder();
 								stringDigiteValoresMoedaValidos.append("Erro ao adicionar moeda!" +
 								"Só podem ser adicionados os seguintes valores de " + 
-								moeda.getNomeMoeda() + ": ");
+								moeda.getNomeMoeda() + ": "); //buscar nome da moeda 
+								//monta string apartir da lista de valores  válidos para essa moeda 
 								String somenteValoresValidos = moedaValoresValidos.stream()
 		                                  .map(Object::toString)
 		                                  .collect(Collectors.joining(", "));
 								stringDigiteValoresMoedaValidos.append(somenteValoresValidos);
 								System.out.println(stringDigiteValoresMoedaValidos);
 							}
-							break;
+							break; //sair desse loop 
 						}
 					}
 				}
 				else if (opcaoMenuPrincipal == 2) { //remover moeda
-					while (true) {
+					while (true) { //executar em loop até que seja selecionada uma opção válida 
 						System.out.println("Digite a opção desejada: \n 1-Real \n 2-Dólar \n 3-Euro");
-						moedaEscolhida = scanner.nextInt();
-						System.out.println("Digite o valor:");
+						moedaEscolhida = scanner.nextInt(); //ler a opção digitada pelo usúario 
+						System.out.println("Digite o valor:"); 
 						try {
-							valor = scanner.nextDouble();
+							valor = scanner.nextDouble();  //ler o valor digitado pelo usúario 
 						} 
-						catch (InputMismatchException e){
+						catch (InputMismatchException e){ // tratamento de erro para  caso o usúario digite um valor inválido
 							System.out.println("O valor digitado é inválido, digite apenas números. " + 
 											  "Utilize a vírgula como separador decimal");
-							scanner.nextLine();
-							continue;
+							scanner.nextLine(); //para avançar uma linha no scanner e não dar erro 
+							continue; // //voltando ao inicio do loop 
 						}
 						if (moedaEscolhida == 1) {
-							moeda = new Real(valor);
+							moeda = new Real(valor); //cria uma moeda de Real com o valor passado pelo usúario 
 						}
 						else if (moedaEscolhida == 2) {
-							moeda = new Dolar(valor);
+							moeda = new Dolar(valor); //cria uma moeda de Dolar com o valor passado pelo usúario 
 						}
 						else if (moedaEscolhida == 3) {
-							moeda = new Euro(valor);
+							moeda = new Euro(valor); //cria uma moeda de Euro com o valor passado pelo usúario 
 						}
 						
-						if (moedaEscolhida >= 1 && moedaEscolhida <=3) {
-							Boolean removidaComSucesso = cofrinho.remover(moeda);
-							if (removidaComSucesso == true) {
+						if (moedaEscolhida >= 1 && moedaEscolhida <=3) {//se foi selecionada uma opção válida 
+							Boolean removidaComSucesso = cofrinho.remover(moeda);//remove moeda do cofrinho 
+							if (removidaComSucesso == true) { //verifica se moeda foi adicionada com sucesso no cofrinho 
 								System.out.println("Moeda removida com sucesso!");
 							}
 							else {
 								System.out.println("Erro ao remover moeda! Nenhuma moeda do valor informado está no cofrinho.");
 							}
-							break;
+							break;//sair desse loop 
 						}
 					}
 				}
 				else if (opcaoMenuPrincipal == 3) { //listar moedas
-					String listagemMoedas = cofrinho.listagemMoedas();
-					System.out.println(listagemMoedas);
+					String listagemMoedas = cofrinho.listagemMoedas();//obter lista de moedas que estão no cofrinho
+					System.out.println(listagemMoedas); //exibir para o usúario  a lista de moedas que estão no cofrinho 
 				}
 				else if (opcaoMenuPrincipal == 4) { //calcular total convertido
-					Double totalConvertidoParaReal = cofrinho.totalConvertido();	
+					//calcular total das moedas do cofrinho convertidas para Real 
+					Double totalConvertidoParaReal = cofrinho.totalConvertido(); 
+					//ixibi para usúario total das moedas do cofrinho convertidas para Real 
 					System.out.println("Total convertido para reais: R$" + totalConvertidoParaReal);
 				}
 				else if (opcaoMenuPrincipal == 0) { //encerrar
-					break;
+					break;//sair desse loop e encerrar o programa 
 				}
 					
 			}
-		} catch (Exception e){
+		} catch (Exception e){ //tratamento de erro caso ocorra um erro inesperado no programa 
 			System.out.println("Erro inesperado no sistema cofrinho. O sistema será encerrado.");
 		} finally {
-			scanner.close();
+			scanner.close();//liberar o objeto Scanner da memória 
 		}
 			
 	}
